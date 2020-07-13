8504dadd348d31eb50fd29990e294711a79288d3
teoitinga <teo.itinga@gmail.com>
2020-07-12 23:52:01 -0300

    começando do zerogit status!

--- src/main/java/com/jp/eslocapi/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jp.eslocapi.repositories;
-
-import com.jp.eslocapi.api.entities.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
